d408b200a9b4d015b077e9addf721eeb377e61b6
John Watson <john@watson-net.com>
2012-06-06 15:50:47 -0700

    Added searching within the community and local folders. Added completions for Magento's factory classes Mage::getMethod(), Mage::getSingleton(), Mage::helper(), and Mage::app(). Added a command to go to the file where a class is declared. Added README and AUTHORS file.

---
+++ README.md
@@ -0,0 +1,46 @@
+# Magento Intel
+
+This is a naive completion plugin for Magento.
+
+Given a type hint or a factory method, and based on an understanding of Magento class autoloading, the plugin will find the source file in the current project, scan it for function definitions, and add them to the auto complete popup.
+
+This allows you to complete in situations like this:
+
+    /* @var $v Mage_Catalog_Model_Product */
+    $v->{auto complete}
+
+and even:
+
+    Mage::getModel('catalog/product')
+        ->load(100)
+        ->{auto complete}
+
+It also understands some Magento factories:
+
+    Mage::getModel('catalog/product')->{auto complete}
+    Mage::getSingleton('catalog/product')->{auto complete}
+    Mage::helper('sales')->{auto complete}
+
+Auto complete by pressing `Ctrl/Cmd+space` immediately following ->. Select one of the choices and you'll even get parameters you can tab through.
+
+This is all done dynamically so nothing needs to be scanned before the system starts working. And it's quite fast because it only has to stat a few files on each invocation.
+
+It also includes a handy function for opening the source file for any class: _magento_open_. Search the command palette for Magento or bind it to a key. For example:
+
+    [
+        {"keys": ["ctrl+shift+o"], "command": "magento_open" },
+    ]
+
+The command is only active within Magento projects.
+
+# Limitations
+
+- This should be considered an early alpha.
+- You must be using a project and the root of your project must contain the Magento app folder.
+- It only understands _@var $var type_ references and does not go any further than the referenced class (it doesn't follow the inheritance tree).
+- It doesn't understand doc strings, @param, class @var, or @return hints. So, the completion only really works at one level.
+- It doesn't understand the language so it fails in spectacular fashion in a number of situations, especially when chaining. Check the class names in the auto-complete popup to be sure you're getting completions for the right thing.
+- It always assumes that the previous function returns $this for chaining like $model->method1()->method2().
+- It uses a naive algorithm for deciding what the class name is for factory methods like Mage::getModel() and Mage::helper(). It does not inspect Magento config.xml files.
+
+Despite all that, it can still be quite handy. Please send pull requests.
--- magentointel.py
+++ magentointel.py
@@ -1,35 +1,37 @@
 '''
+MagentoIntel plugin for Sublime Text 2
 Copyright 2012 John Watson <https://github.com/jotson>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 '''
 
 import os
 import re
+import glob
+import copy
 import sublime
 import sublime_plugin
 
 
 class MagentoComplete(sublime_plugin.EventListener):
     '''
-    This is a naive completion plugin for Magento.
-
-    Given a type hint, and based on an understanding of Magento class
-    autoloading, the plugin will find the source file referenced by
-    a variable in the current project, scan it for function definitions,
-    and add them to the auto complete popup.
-
-    This is all done dynamically so nothing needs to be scanned before
-    the system starts working. And it's quite fast because it only has
-    to stat a couple of files on each invocation.
-
-    This version only understands @var $var type references and does
-    not go any further than the referenced class (it doesn't follow
-    the inheritance tree).
-
-    It also doesn't understand doc strings, @param, class @var or
-    @return values. So, the completion only works at one level.
-
-    Despite that, it can still be quite handy for discovering methods
-    available in the Magento core classes.
+    Magento auto-completer.
     '''
 
     def on_query_completions(self, view, prefix, locations):
@@ -50,29 +52,27 @@ class MagentoComplete(sublime_plugin.EventListener):
         Scan files for completions for the current context
         '''
 
-        '''Get variable name to be completed'''
+        '''Get token to be completed'''
         point = view.sel()[0].a
-        source = view.substr(sublime.Region(point - 100, point))
-        source = source.split('->')
-        source = source[len(source) - 2]
-        source = source.split('\n')
-        source = source[len(source) - 1]
-        source = source.split(' ')
-        source = source[len(source) - 1]
-        source = source.split('\t')
-        source = source[len(source) - 1]
-        if not source:
-            return data
+        tokens = view.substr(sublime.Region(point - 1000, point))
+        tokens = re.split('->|\n|\r|\t| ', tokens)
+        tokens = tokens[:len(tokens) - 1]
+        tokens.reverse()
 
-        '''Scan current file for @var $source to get class'''
-        searchtext = source.replace('$', '\$')
-        searchtext += ' .* '
-        found = view.find('@var ' + searchtext, 0)
-        if not found:
-            return data
+        className = None
+        for token in tokens:
+            '''Convert the token to a class name'''
+            if token:
+                if token.find(';') >= 0:
+                    '''
+                    Stop at ANY semicolon. This is a bad tokenizer in need of help.
+                    '''
+                    break
 
-        definition = view.substr(found).lstrip().rstrip()
-        className = definition.split(' ')[2]
+                className = self.convert_token(view, token)
+                if className:
+                    break
+
         if not className:
             return data
 
@@ -100,7 +100,47 @@ class MagentoComplete(sublime_plugin.EventListener):
 
             data.append(tuple([f + '\t' + className, snippet]))
 
-        return data
+        return sorted(data)
+
+    def convert_token(self, view, token):
+        '''
+        Given a token, convert it into a Magento class name.
+
+        If the token is a variable (e.g. $var) then we search for @var hints.
+        If the token is a Magento factory class, then convert those to Mage_*
+            class names.
+        '''
+        className = ''
+        token = token.strip()
+        if token.startswith('$'):
+            searchtext = token.replace('$', '\$')
+            searchtext = searchtext.replace('(', '\(')
+            searchtext = searchtext.replace(')', '\)')
+            searchtext += ' .* '
+            found = view.find('@var ' + searchtext, 0)
+            if not found:
+                return None
+
+            definition = view.substr(found).strip()
+            className = definition.split(' ')[2]
+            if not className:
+                return None
+        elif token.startswith('Mage::getModel') or token.startswith('Mage::getSingleton'):
+            key = re.findall("\('(.*)'\)", token)
+            (module, theclass) = key[0].split('/')
+            className = 'Mage_{m}_Model_{c}'.format(m=cap_first_letter(module), c=cap_first_letter(theclass))
+            pass
+        elif token.startswith('Mage::helper'):
+            key = re.findall("\('(.*)'\)", token)
+            module = key[0]
+            className = 'Mage_{m}_Helper_Data'.format(m=cap_first_letter(module))
+            pass
+
+        elif token.startswith('Mage::app'):
+            className = 'Mage'
+            pass
+
+        return className
 
     def scan_file(self, file):
         '''
@@ -134,7 +174,9 @@ class MagentoComplete(sublime_plugin.EventListener):
 
     def build_magento_path(self, className):
         '''
-        Build a file path given a Magento class name
+        Build a file path given a Magento class name.
+
+        Searches in core, lib, local, and community.
         '''
         path = None
 
@@ -146,10 +188,17 @@ class MagentoComplete(sublime_plugin.EventListener):
             return
 
         '''
-        Look for the class file in app/code then lib
+        Look for the class file in various locations
         '''
         if path == None:
             path = [root, 'app', 'code', 'core']
+            path.extend(className.split('_'))
+            path = os.path.join(*path) + '.php'
+            if not os.path.isfile(path):
+                path = None
+
+        if path == None:
+            path = [root, 'app']
             path.extend(className.split('_'))
             path = os.path.join(*path) + '.php'
             if not os.path.isfile(path):
@@ -162,9 +211,54 @@ class MagentoComplete(sublime_plugin.EventListener):
             if not os.path.isfile(path):
                 path = None
 
+        if path == None:
+            '''Try community and local folders'''
+            folders = ([root, 'app', 'code', 'local'], [root, 'app', 'code', 'community'])
+            for path in folders:
+                path.extend(className.split('_'))
+                path = os.path.join(*path) + '.php'
+                if os.path.isfile(path):
+                    break
+                else:
+                    path = None
+
+        if path == None:
+            '''
+            Imagine an invocation like this: Mage::getModel('module/example').
+            The parser will generate a class name of Mage_Module_Model_Example.
+            If that class isn't found in any of the above scopes, then we try
+            this more intensive search:
+
+            We'll look in all of the local and community folders.
+            We'll iterate over all of the folders in each of those folders and
+                replace the Mage_ part of the class name with the folder.
+                Mage_Module_Model_Example => Folder_Module_Model_Example
+            Then we check if that class exists and stop after the first match.
+            '''
+            folders = ([root, 'app', 'code', 'local'], [root, 'app', 'code', 'community'])
+            for origpath in folders:
+                newpath = copy.copy(origpath)
+                newpath.append('*')
+                for folder in glob.glob(os.path.join(*newpath)):
+                    c = className.split('_')
+                    if c[0] == 'Mage':
+                        c[0] = os.path.basename(folder)
+                    newpath = copy.copy(origpath)
+                    newpath.extend(c)
+                    path = os.path.join(*newpath) + '.php'
+                    if os.path.isfile(path):
+                        break
+                    else:
+                        path = None
+                if path:
+                    break
+
         return path
 
     def get_root_folder(self):
+        '''
+        Get the root Magento folder
+        '''
         root = None
         for f in sublime.active_window().folders():
             if os.path.isdir(os.path.join(f, 'app', 'code', 'core', 'Mage')):
@@ -172,3 +266,30 @@ class MagentoComplete(sublime_plugin.EventListener):
                 break
 
         return root
+
+
+class MagentoOpenCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        view = self.window.active_view()
+        className = view.substr(view.sel()[0])
+        path = None
+        if className:
+            path = MagentoComplete.build_magento_path(MagentoComplete(), className)
+        if path:
+            self.window.open_file(path)
+        else:
+            sublime.status_message('Select a class name first')
+
+    def is_enabled(self):
+        return is_magento()
+
+
+def cap_first_letter(word):
+    return word[0].upper() + word[1:]
+
+
+def is_magento():
+    for f in sublime.active_window().folders():
+        if os.path.isdir(os.path.join(f, 'app', 'code', 'core', 'Mage')):
+            return True
+    return False
